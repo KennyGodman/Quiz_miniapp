8137b69704287b6c76c6b793bc76a3bef4ecb395
Kehinde <kennydaniels138@gmail.com>
2022-12-24 14:58:13 +0100

    first commit

---
+++ src/QuizzData.js
@@ -0,0 +1,57 @@
+const questions = [
+  {
+    questionText: "How many hours are in a day",
+    answerOptions: [
+      { answerText: "2", isCorrect: false },
+      { answerText: "9", isCorrect: false },
+      { answerText: "24", isCorrect: true },
+      { answerText: "2", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many planets are on earth",
+    answerOptions: [
+      { answerText: "4", isCorrect: false },
+      { answerText: "9", isCorrect: true },
+      { answerText: "2", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many continents are in the world",
+    answerOptions: [
+      { answerText: "2", isCorrect: false },
+      { answerText: "9", isCorrect: false },
+      { answerText: "7", isCorrect: true },
+      { answerText: "4", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which is not a Designer",
+    answerOptions: [
+      { answerText: "Nike", isCorrect: false },
+      { answerText: "Diin", isCorrect: true },
+      { answerText: "Addidas", isCorrect: false },
+      { answerText: "Puma", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Which of these is not a programming language",
+    answerOptions: [
+      { answerText: "Java", isCorrect: false },
+      { answerText: "hyn", isCorrect: true },
+      { answerText: "Python", isCorrect: false },
+      { answerText: "Ruby", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "How many letters are there in GOOD",
+    answerOptions: [
+      { answerText: "4", isCorrect: false },
+      { answerText: "3", isCorrect: true },
+      { answerText: "2", isCorrect: false },
+      { answerText: "6", isCorrect: false },
+    ],
+  },
+];
+export default questions;
